c9c50ce4ada62af05f08a0ab71209d1ca92f4267
Tosin17 <kajopelayeola@gmail.com>
2019-04-04 09:04:22 -0400

    Build API client

---
+++ ApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace MultiplyTwoMatrices
+{
+    public static class ApiClient {
+        public static HttpClient client = new HttpClient();
+
+        public static void initializeApiClient() {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://recruitment-test.investcloud.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ Logs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultiplyTwoMatrices
+{
+    public static class Logs {
+        public static void LogMatrixRow(MatrixRowModel row) {
+            Console.WriteLine($"Value: {row.Value}");
+            foreach (var i in row.Value)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void LogInitializeResponse(PostMatrixModel init) {
+            Console.WriteLine($"Initialized: {init.Value}");
+        }
+
+        public static void LogPostResponse(PostMatrixModel post) {
+            Console.WriteLine($"Posted: {post.Value}");
+        }
+    }
+}
