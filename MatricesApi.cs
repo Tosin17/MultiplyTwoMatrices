e88a87dbeeb465b1442b9efe4619e4fbbcba6180
Tosin17 <kajopelayeola@gmail.com>
2019-04-04 21:34:33 -0400

    Add matric DTO

---
+++ MatricesApi.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace MultiplyTwoMatrices
+{
+    public static class MatricesApi {
+        public static async Task<MatrixRowModel> GetMatrixAsync(string path, int rowIndex) {
+            MatrixRowModel row = new MatrixRowModel();
+
+            HttpResponseMessage response = await ApiClient.client.GetAsync(path);
+            if (response.IsSuccessStatusCode) {
+                row = await response.Content.ReadAsAsync<MatrixRowModel>();
+            }
+            row.RowIndex = rowIndex;
+            return row;
+        }
+
+        public static async Task<MatrixInitializeModel> InitMatrixAsync(string path) {
+            MatrixInitializeModel init = new MatrixInitializeModel();
+
+            HttpResponseMessage response = await ApiClient.client.GetAsync(path);
+            if (response.IsSuccessStatusCode) {
+                init = await response.Content.ReadAsAsync<MatrixInitializeModel>();
+            }
+            return init;
+        }
+
+        public static async Task<PostMatrixModel> PostMatrixAsync(string path, string hash) {
+            PostMatrixModel post = new PostMatrixModel();
+
+            HttpResponseMessage response = await ApiClient.client.PutAsJsonAsync(path, hash);
+            response.EnsureSuccessStatusCode();
+
+            post = await response.Content.ReadAsAsync<PostMatrixModel>();
+
+            return post;
+        }
+    }
+}
---
+++ MatrixRowModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultiplyTwoMatrices
+{
+    public class MatrixRowModel {
+        public int[] Value { get; set; }
+
+        public int RowIndex { get; set; } = 0;
+
+        public string Cause { get; set; }
+
+        public bool Success { get; set; }
+    }
+
+}
